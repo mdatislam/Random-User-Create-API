da8381e54dd53a751d8d823e1a604196d9ccdf8d
unknown <mdatislam>
2023-05-09 08:36:50 +0600

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//Midle ware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    // console.log("Hello Random User API")
+    res.send('welcome Random User Create API')
+})
+
+app.all('*', (req,res) => {
+    res.send(" Route Not found")
+})
+
+app.listen(port, () => {
+    console.log(`App Listening on Port ${port}`)
+})
+
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
+
 let 
 user = [
     {id:1, Name:"Atiqul", gender:"Male"}
